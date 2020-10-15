895087f7c70c1b2b8230e1d2ab2ec38417fe1b17
Miguel Henrique <miguelhp373@gmail.com>
2020-10-15 18:08:32 -0300

    Mobile Version - Dia 04 Completo

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+import {useFonts} from 'expo-font';
+import {Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito';
+
+import Routes from './src/routes'
+
+
+
+
+export default function App() {
+  const [fontsloaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  })
+
+  if(!fontsloaded){
+    return null;
+    }
+return(
+  <Routes/>
+);
+ 
+
+
+};
---
+++ mobile/src/pages/OphanagesDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+
+export default function OrphanageDetails(){
+    return(
+        <View>
+            <Text>Teste 12</Text>
+        </View>
+    )
+}
